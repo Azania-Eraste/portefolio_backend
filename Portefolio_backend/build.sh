541dd113059c1578fcf7ab9637bde54726b5a262
Azania-Eraste <kouadioazania@gmail.com>
2026-06-12 14:10:10 +0200

    Remove migrations from build.sh to prevent deploy failures on DB connection issues

--- Portefolio_backend/build.sh
+++ Portefolio_backend/build.sh
@@ -6,6 +6,3 @@ pip install -r requirements.txt
 
 # Collecter les fichiers statiques (CSS Admin, etc.)
 python manage.py collectstatic --no-input
-
-# Appliquer les migrations à la base de données
-python manage.py migrate
